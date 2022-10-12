3eabcd05dd83c594c22a4ecbb53ac8e824c46cf3
Varshini <varshuk01@gmail.com>
2022-10-12 15:19:16 +0530

    quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import{RouterModule,Routes} from '@angular/router';
+import { QuotesdataComponent } from './quotesdata/quotesdata.component';
+const appRoutes:Routes=[
+  {
+   path:"",component:QuotesdataComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuotesdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
